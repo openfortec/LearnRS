0a061418ee0749f04c984909e8d09d0d9a7d64f3
openfortec <openfortec@gmail.com>
2014-09-21 21:18:26 +0530

    added pathparameter with testcase
    test case added in soapui to validate path parameter value

--- src/main/java/learn/sanga/rs/sample/MySecondRsService.java
+++ src/main/java/learn/sanga/rs/sample/MySecondRsService.java
@@ -2,15 +2,16 @@ package learn.sanga.rs.sample;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/secondrs")
+@Path("/secondrs/{name}")
 public class MySecondRsService {
 	@GET
 	@Produces(MediaType.TEXT_HTML)
-	public String getMessage() {
-	
-		return "<p>Hello second RESTFUL Web Service</p>";
+	public String getMessage(@PathParam("name") String name) {
+		System.out.println("i am called" +name);
+		return "<p>Welcome "+name+" to second example</p>";
 	}
 }
